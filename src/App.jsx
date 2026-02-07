f1c0bde75d2f15f3bbc2c79fcc1b5784f3dffe44
bharath88979-byte <bharath88979@gmail.com>
2026-02-08 00:42:57 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -180,7 +180,7 @@ export default function App() {
   );
 
   const questionText3 = useInfiniteTypewriter(
-    "Happy Valentine’s Day, My Love #RGForever 💖",
+    "Happy Valentine’s Day, My Love #TUSHITHA#Forever 💖",
     70,
     2000
   );
